1168147d0ae5b908bed53ea3268bef2984b860f9
seokjoon <seokjoon@gmail.com>
2018-01-27 18:38:31 +0900

    init

---
+++ components/com_EcfirmNetBase/View/EcListHtmlView.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package ecfirm.net
+ * @copyright Copyright (C) ecfirm.net. All rights reserved.
+ * @license GNU General Public License version 2 or later.
+ */
+
+namespace Joomla\Component\EcfirmNetBase\Site\View;
+
+defined('_JEXEC') or die;
+
+class EcListHtmlView extends EcHtmlView
+{
+
+}
